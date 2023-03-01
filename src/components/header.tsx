478cb177e3bc0809d7880502caa269f5babc23f5
Pradit Yamsook <pradit.y@ku.th>
2023-03-01 19:52:54 +0700

    create header components

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined, HomeOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import type { FC } from 'react';
+import { Menu } from 'antd';
+
+const items: MenuProps['items'] = [
+    {
+        label: "Home",
+        key: "home",
+        icon: <HomeOutlined />
+    }
+]
+
+
+const AppHeader: FC = () => (
+    const [current, setCurrent] = useState('home');
+    const onClick: MenuProps['onClick'] = (e) => {
+        setCurrent(e.key);
+    };
+
+    return (
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+    );
+)
+
+
+export default AppHeader;
