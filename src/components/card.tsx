01d9e29301780029cbc4610e48a39cca1028b951
Pradit Yamsook <pradit.y@ku.th>
2023-03-02 23:42:40 +0700

    adjust responsive

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,26 +1,28 @@
 import { Card, Row, Col } from 'antd';
+import axios from 'axios';
 import type { FC } from 'react';
-import { imageCard } from '../API/image'
+// import { getImage } from '../API/image'
+// import { imageCard } from '../state/recoil_state';
 
 import './styles/card.css';
 
-
+const imageName = ['jocelyn', 'jaqueline', 'jed', 'jabala', 'jacques', 'jack', 'jeri', 'josh', 'josephine', 'jake', 'jana', 'jenni', 'jolee', 'jai', 'jess', 'joe', 'jeane', 'jon', 'jazebelle', 'jean', 'jane', 'jodi', 'james', 'jordan', 'jerry', 'julie', 'jude', 'jia']
 const { Meta } = Card;
-// let image = "https://joesch.moe/api/v1/random";
-// let images: image[] = imageCard("https://picsum.photos/v2/list");
-// console.log(images[0])
-// let getImage = imageCard("https://picsum.photos/v2/list");
+
 let cardList: any[] = [];
 for (let i = 0; i < 20; i++) {
     cardList.push(
-        <Col span={4} style={{
-            marginTop: 20
-        }}>
+        <Col xs={{ span: 24, offset: 0 }}
+            lg={{ span: 8, offset: 0 }}
+            md={{ span: 12, offset: 0 }}
+            xl={{ span: 6, offset: 0 }}
+            className={'col-product-card'}
+        >
             <Card
                 bordered={true}
                 hoverable
-                style={{ width: 300 }}
-                cover={<img alt="example" src="https://joesch.moe/api/v1/random" style={{ width: 300, height: 400 }} />}
+                className={'product-card'}
+                cover={<img alt="example" src={`https://joesch.moe/api/v1/${imageName[i]}`} className={'img-product-card'} />}
             >
                 <Meta title="Europe Street beat" description="www.instagram.com" />
                 <div style={{ marginBottom: 8 }} />
@@ -37,9 +39,12 @@ for (let i = 0; i < 20; i++) {
     );
 }
 const CardProducts: FC = () => (
-    <Row gutter={16}>
-        {cardList}
-    </Row>
+    <div className='div-res-product-card'>
+        <Row>
+            {cardList}
+        </Row>
+    </div>
+
 );
 
 export default CardProducts;
