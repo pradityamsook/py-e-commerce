2aa8333b65ccae5db8827690c95577e0066f6562
Pradit Yamsook <pradit.y@ku.th>
2023-03-02 15:24:37 +0700

    add app store from menu property

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useRecoilState, atom, useRecoilValue } from 'recoil';
 import { AppstoreOutlined, MailOutlined, SettingOutlined, HomeOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import type { FC } from 'react';
+import { useState } from 'react';
 import { Menu } from 'antd';
 
 const items: MenuProps['items'] = [
     {
         label: 'Home',
         key: 'home',
-        icon: <HomeOutlined />
+        icon: <HomeOutlined style={{ fontSize: "30px"}} />
+    },
+    {
+        label: 'App Store',
+        key: 'appStore',
+        icon: <AppstoreOutlined style={{fontSize: "30px"}} />
     }
 ];
+
 
 const AppHeader: FC = () => {
     const [current, setCurrent] = useState('home');
@@ -19,7 +26,7 @@ const AppHeader: FC = () => {
     };
 
     return (
-        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} style={{ fontSize: "30px", marginTop: "20px"}} />
     );
 };
 
