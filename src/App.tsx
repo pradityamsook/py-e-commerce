822683469b1c518077cede6a790fc9fb5a97a921
Pradit Yamsook <pradit.y@ku.th>
2023-03-02 15:23:41 +0700

    add card product component in body.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import type { FC } from 'react';
 import { Button } from 'antd';
 import AppHeader from './components/header';
+import CardProducts from './components/card';
 import 'antd/dist/reset.css';
 import './App.css';
 
 const buttonApp: FC = () => (
-    <div className='App'>
-    <Button type="primary">Button</Button>
+  <div className='App'>
     <AppHeader />
+    <body>
+      <div style={{ marginTop: 50 }}>
+        <CardProducts />
+      </div>
+    </body>
   </div>
 )
 
