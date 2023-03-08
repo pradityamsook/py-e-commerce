eaf22866708a80103a8cc5508142058979323ac2
Pradit Yamsook <pradit.y@ku.th>
2023-03-08 23:55:00 +0700

    add useRecoilValue in product detail

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -1,17 +1,22 @@
 import { Row, Col } from "antd";
 import type { FC } from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { productState } from "../state/recoil_state";
 
 import "./styles/product-detail.css"
 
 const ProductDetail: FC = () => {
-    const productName = useParams;
-
+    const { productName, index }: any = useParams();
+    const products: any = useRecoilValue(productState)
+    const getIndex = parseInt(index)
     return (
         <div>
             <Row>
                 <Col span={12} offset={6} className={"image-center"}>
                     <img src={`https://joesch.moe/api/v1/${productName}`} className={"img-detail"}></img>
+                    <div>{products[getIndex].name}</div>
+                    <div>Amount: {products[getIndex].amount}</div>
                 </Col>
             </Row>
         </div>
